991ce12658dc99fe097ced3f2a0c94ddd048387f
Sam <sam.terrazas@yahoo.com>
2019-03-24 16:02:55 -0400

    one more time. missing } in connection.js line 38

--- config/connection.js
+++ config/connection.js
@@ -1,43 +1,40 @@
 // MYSQL CONNECTION
 var mysql = require("mysql");
-
 var connection;
 
-// // USING JAWSDB TO CONFIGURE CONNECTION
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   // MYSQL DB CONNECTION INFO
-//   var connection = mysql.createConnection({
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: 'password',
-//     database: 'burgers_db'
-//   });
-// }
 
-// MYSQL DB CONNECTION INFO
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'burgers_db'
-});
+// USING JAWSDB TO CONFIGURE CONNECTION
+if (process.env.JAWSDB_URL) {
+  console.log("I'm utilizing JAWSDB");
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // MYSQL DB CONNECTION INFO
+  console.log("Using local DB");
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db'
+  });
 
-// INIT MYSQL CONNECTION
-connection.connect(function (err) {
-  if (err) {
-    console.log("Error connecting to MySQL: " + err.stack);
-    return;
-  }
-  console.log("MySQL connected as id " + connection.threadId);
-});
+  // // MYSQL DB CONNECTION INFO
+  // var connection = mysql.createConnection({
+  //   host: 'localhost',
+  //   port: 3306,
+  //   user: 'root',
+  //   password: 'password',
+  //   database: 'burgers_db'
+  // });
 
+  // INIT MYSQL CONNECTION
+  connection.connect(function (err) {
+    if (err) {
+      console.log("Error connecting to MySQL: " + err.stack);
+      return;
+    }
+    console.log("MySQL connected as id " + connection.threadId);
+  });
+}
 // EXPORT CONNECTION FOR OUR ORM
 module.exports = connection;
-
-
-
-
